331a1241473ccb63ac6e5fec7cd4e34a8632a825
luminisward <luminisward@icloud.com>
2023-11-18 15:02:02 +0800

    add request time

--- src/main/java/xyz/monado/subs/RequestUtil.java
+++ src/main/java/xyz/monado/subs/RequestUtil.java
@@ -1,5 +1,7 @@
 package xyz.monado.subs;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
@@ -10,14 +12,20 @@ import java.net.URI;
 
 @Component
 public class RequestUtil {
+    private static final Logger logger = LoggerFactory.getLogger(LoggingInterceptor.class);
     private final WebClient webClient;
 
     public RequestUtil(WebClient webClient) {
         this.webClient = webClient;
     }
+
     @Cacheable(value = "request", key = "#uri.toString() + #clientType")
     public ResponseEntity<String> requestRemoteConfig(URI uri, ClientType clientType) {
         var requestBuilder = webClient.get().uri(uri);
-        return requestBuilder.header(HttpHeaders.USER_AGENT, clientType.name()).retrieve().toEntity(String.class).block();
+        long start = System.currentTimeMillis();
+        var result = requestBuilder.header(HttpHeaders.USER_AGENT, clientType.name()).retrieve().toEntity(String.class).block();
+        long end = System.currentTimeMillis();
+        logger.info("Time: {}, URI: {}", String.format("%sms", end - start), uri);
+        return result;
     }
 }
